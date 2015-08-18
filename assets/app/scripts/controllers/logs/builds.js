34e3867cc6636aebd169cedb6650fb8e42ff08de
benjaminapetersen <admin@benjaminapetersen.me>
2015-08-18 11:08:43 -0400

    various log updates

--- assets/app/scripts/controllers/logs/builds.js
+++ assets/app/scripts/controllers/logs/builds.js
@@ -8,14 +8,8 @@ angular.module('openshiftConsole')
     'DataService',
     function($log, $routeParams, $scope, DataService) {
 
-      console.log('$scope is', $scope);
       DataService.watch('builds', $scope, function(builds) {
-
-
-        console.log('$scope is...', $scope);
-
         angular.extend($scope, {
-          // projectName is provided by mixing in ProjectController in the template
           builds: builds.by('metadata.name'),
           emptyMessage: 'No builds to show'
         });
